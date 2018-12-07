46754ae21427d808c8c467092fe8fe4cfa53bb94
crognier <nemo.crognier@gmail.com>
2018-12-07 15:07:48 +0100

    gestion du calendrier (date calendar, date fin cursus etc)

--- src/main/webapp/app/entities/cursus/cursus-detail.component.ts
+++ src/main/webapp/app/entities/cursus/cursus-detail.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
-import { ICursus } from 'app/shared/model/cursus.model';
+import { Cursus, ICursus } from 'app/shared/model/cursus.model';
+import moment = require('moment');
 
 @Component({
     selector: 'jhi-cursus-detail',
     templateUrl: './cursus-detail.component.html'
 })
+
 export class CursusDetailComponent implements OnInit {
     cursus: ICursus;
+    dureeCalendar: number;
+    dateDeFin: Date;
 
-    constructor(private activatedRoute: ActivatedRoute) {}
+    constructor(private activatedRoute: ActivatedRoute) {
+    }
 
     ngOnInit() {
 
@@ -21,5 +26,38 @@ export class CursusDetailComponent implements OnInit {
 
     previousState() {
         window.history.back();
+    }
+
+    calculerDureeCalendar(cursus: Cursus) {
+        let duree: number;
+        let rest: number;
+        duree = cursus.duree;
+        this.dureeCalendar = (Math.trunc(duree / 5) * 2) + duree;
+        this.dateDeFin = moment(cursus.dateDebut).add(((this.dureeCalendar) - 1), 'd').format('YYYY-MM-DD-dd');
+        rest = duree % 5;
+
+        if (rest === 0) {
+            if ((moment(this.dateDeFin).isoWeekday()) !== 6 || 7) {
+                this.dateDeFin = moment(cursus.dateDebut).add(((this.dureeCalendar) - 1), 'd').format('YYYY-MM-DD-dd');
+            } else if ((moment(this.dateDeFin).isoWeekday()) === 6) {
+                this.dateDeFin = moment(cursus.dateDebut).add(((this.dureeCalendar) - 1 + 2), 'd').format('YYYY-MM-DD-dd');
+            } else if ((moment(this.dateDeFin).isoWeekday()) === 7) {
+                this.dateDeFin = moment(cursus.dateDebut).add(((this.dureeCalendar) - 1 + 2), 'd').format('YYYY-MM-DD-dd');
+            }
+        } else if (rest !== 0) {
+            if ((moment(this.dateDeFin).isoWeekday()) !== 6 || 7) {
+                this.dateDeFin = moment(cursus.dateDebut).add(((this.dureeCalendar)), 'd').format('YYYY-MM-DD-dd');
+            } else if ((moment(this.dateDeFin).isoWeekday()) === 6) {
+                this.dateDeFin = moment(cursus.dateDebut).add(((this.dureeCalendar) + 2), 'd').format('YYYY-MM-DD-dd');
+            } else if ((moment(this.dateDeFin).isoWeekday()) === 7) {
+                this.dateDeFin = moment(cursus.dateDebut).add(((this.dureeCalendar) + 2), 'd').format('YYYY-MM-DD-dd');
+            }
+        }
+        console.log('cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc');
+        console.log(moment(cursus.dateDebut).add(this.dureeCalendar, 'd').format('YYYY-MM-DD-dd'));
+        console.log('cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc');
+        console.log(moment(cursus.dateDebut).isoWeekday());
+
+        return this.dureeCalendar;
     }
 }
