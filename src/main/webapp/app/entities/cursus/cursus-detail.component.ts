231f349c77af0db14b994e7d87eab2a26bf0bfb8
Tdriant <thomas@driant.org>
2018-12-07 15:18:39 +0100

    correction de bugs

--- src/main/webapp/app/entities/cursus/cursus-detail.component.ts
+++ src/main/webapp/app/entities/cursus/cursus-detail.component.ts
@@ -2,11 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { ICursus } from 'app/shared/model/cursus.model';
-import { IModule, Module } from 'app/shared/model/module.model';
-import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
-import { IUser } from 'app/core';
-import { ModuleService } from 'app/entities/module';
-import { JhiAlertService } from 'ng-jhipster';
+import { Module } from 'app/shared/model/module.model';
+import { Moment } from 'moment';
 import moment = require('moment');
 
 @Component({
@@ -35,7 +32,7 @@ export class CursusDetailComponent implements OnInit {
                console.log(moment(a.dateDebut).toDate().setDate(moment(a.dateDebut).toDate().getDate() + 1));
                console.log(moment(a.dateDebut).toDate().setDate(moment(a.dateDebut).toDate().getDate()));
                console.log(moment(a.dateDebut).add(1, 'd'));
-               console.log(moment(a.dateDebut).add(1, 'd').format("YYYY-MM-DD"));
+               console.log(moment(a.dataaeDebut).add(1, 'd').format('YYYY-MM-DD'));
                console.log(a.dateDebut.add(1, 'd'));
                console.log('après ça bugge');
                console.log(b);
@@ -52,9 +49,9 @@ export class CursusDetailComponent implements OnInit {
     createMapModules() {
         let j = 0;
         for (let i = 0; i < this.dureeCalendar; i++) {
-            console.log(this.listDate[i])
+            console.log(this.listDate[i]);
             if (this.listDate[i] === this.cursus.modules[j]) {
-                for ( let k = 0; k < this.cursus.modules[j].duree; k++) {
+                for (let k = 0; k < this.cursus.modules[j].duree; k++) {
                     if (this.listDate[i].isoWeekday() in [6, 7]) {
                         this.dateModuleMap.set(this.listDate[i], null);
                     } else {
